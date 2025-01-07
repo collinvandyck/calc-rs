dca08e37b0fee2d7725fdf60560fa2534307236a
Collin Van Dyck <collinvandyck@gmail.com>
2025-01-07 11:37:01 -0500

    wip

---
+++ src/bin/lispy.rs
@@ -0,0 +1,291 @@
+#![allow(unused)]
+
+use anyhow::{Context, Result, bail};
+use core::panic;
+use std::rc::Rc;
+
+fn main() {
+}
+
+type SharedExpr = Rc<Expr>;
+
+#[derive(Debug, Clone)]
+enum Expr {
+    Group(Rc<GroupExpr>),
+    Math(Rc<MathExpr>),
+    Value(Rc<Value>),
+}
+
+impl Expr {
+    fn visit<V: ExprVisitor>(&self, v: &mut V) -> Result<V::Item> {
+        match self {
+            Expr::Group(rc) => v.visit_group(rc.clone()),
+            Expr::Math(rc) => v.visit_math(rc.clone()),
+            Expr::Value(rc) => v.visit_value(rc.clone()),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct MathExpr {
+    op: TokTyp,
+    left: SharedExpr,
+    right: SharedExpr,
+}
+
+#[derive(Debug, Clone)]
+struct GroupExpr {
+    expr: SharedExpr,
+}
+
+#[derive(Clone, Debug)]
+struct Tok {
+    lexeme: String,
+    typ: TokTyp,
+    val: Option<Value>,
+}
+
+impl std::fmt::Display for Tok {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Tok { lexeme, typ, val } = self;
+        if let Some(val) = val {
+            write!(f, "{val}")
+        } else {
+            write!(f, "{lexeme}")
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum TokTyp {
+    LeftParen,
+    RightParen,
+    Plus,
+    Star,
+    Slash,
+    Minus,
+    Number,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+enum Value {
+    F64(f64),
+}
+
+impl Value {
+    fn to_f64(&self) -> Option<f64> {
+        match self {
+            Value::F64(v) => Some(*v),
+        }
+    }
+}
+
+impl std::fmt::Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Value::F64(v) => write!(f, "{v}"),
+        }
+    }
+}
+
+fn eval(s: &str) -> Result<Value> {
+    let expr = parse(s)?;
+    let mut inter = Interpreter::new(expr);
+    inter.interpret()
+}
+
+fn parse(s: &str) -> Result<SharedExpr> {
+    let toks = lex(s)?;
+    Parser::new(toks).parse()
+}
+
+trait ExprVisitor {
+    type Item;
+    fn visit_group(&mut self, group: Rc<GroupExpr>) -> Result<Self::Item>;
+    fn visit_math(&mut self, add: Rc<MathExpr>) -> Result<Self::Item>;
+    fn visit_value(&mut self, val: Rc<Value>) -> Result<Self::Item>;
+}
+
+struct Interpreter {
+    expr: SharedExpr,
+}
+
+impl Interpreter {
+    fn new(expr: SharedExpr) -> Self {
+        Self { expr }
+    }
+    fn interpret(&mut self) -> Result<Value> {
+        self.expr.clone().visit(self)
+    }
+}
+
+impl ExprVisitor for Interpreter {
+    type Item = Value;
+
+    fn visit_group(&mut self, group: Rc<GroupExpr>) -> Result<Self::Item> {
+        group.expr.visit(self)
+    }
+
+    fn visit_math(&mut self, add: Rc<MathExpr>) -> Result<Self::Item> {
+        let MathExpr { left, right, op } = add.as_ref();
+        let left = left.visit(self)?;
+        let right = right.visit(self)?;
+        match (left, right, *op) {
+            (Value::F64(v1), Value::F64(v2), TokTyp::Plus) => Ok(Value::F64(v1 + v2)),
+            (Value::F64(v1), Value::F64(v2), TokTyp::Minus) => Ok(Value::F64(v1 - v2)),
+            (Value::F64(v1), Value::F64(v2), TokTyp::Star) => Ok(Value::F64(v1 * v2)),
+            (Value::F64(v1), Value::F64(v2), TokTyp::Slash) => Ok(Value::F64(v1 / v2)),
+            _ => bail!("invalid math: {add:?}"),
+        }
+    }
+
+    fn visit_value(&mut self, val: Rc<Value>) -> Result<Self::Item> {
+        Ok(val.as_ref().clone())
+    }
+}
+
+struct Parser {
+    toks: Vec<Tok>,
+    pos: usize,
+}
+
+impl Parser {
+    fn new(toks: Vec<Tok>) -> Self {
+        Self { toks, pos: 0 }
+    }
+    fn parse(&mut self) -> Result<SharedExpr> {
+        self.group()
+    }
+    fn group(&mut self) -> Result<SharedExpr> {
+        if self.matches([TokTyp::LeftParen]) {
+            let next = self.math()?;
+            self.consume(TokTyp::RightParen)?;
+            return Ok(Rc::new(Expr::Group(Rc::new(GroupExpr { expr: next }))));
+        }
+        self.math()
+    }
+    fn math(&mut self) -> Result<SharedExpr> {
+        if self.matches([TokTyp::Plus, TokTyp::Minus, TokTyp::Slash, TokTyp::Star]) {
+            let op = self.prev().typ;
+            let left = self.parse()?;
+            let right = self.parse()?;
+            let add = Expr::Math(Rc::new(MathExpr { left, right, op }));
+            return Ok(Rc::new(add));
+        }
+        self.value()
+    }
+    fn value(&mut self) -> Result<SharedExpr> {
+        if self.matches([TokTyp::Number]) {
+            let prev = self.prev();
+            let val = prev.val.context("expected value for number")?;
+            let val = Expr::Value(Rc::new(val));
+            return Ok(Rc::new(val));
+        }
+        bail!("expected value");
+    }
+    fn prev(&self) -> Tok {
+        self.toks[self.pos - 1].clone()
+    }
+    fn consume(&mut self, typ: TokTyp) -> Result<()> {
+        if !self.matches([typ]) {
+            bail!("expected {typ:?}");
+        }
+        Ok(())
+    }
+    fn matches(&mut self, typ: impl IntoIterator<Item = TokTyp>) -> bool {
+        for t in typ.into_iter() {
+            if self.cur().typ == t {
+                self.advance();
+                return true;
+            }
+        }
+        false
+    }
+    fn advance(&mut self) {
+        self.pos += 1;
+    }
+    fn eof(&self) -> bool {
+        self.pos >= self.toks.len()
+    }
+    fn cur(&self) -> &Tok {
+        &self.toks[self.pos]
+    }
+}
+struct Lexer {
+    chars: Vec<char>,
+    toks: Vec<Tok>,
+    pos: usize,
+}
+
+impl Lexer {
+    fn new(s: &str) -> Self {
+        Self {
+            chars: s.chars().collect(),
+            pos: 0,
+            toks: Vec::new(),
+        }
+    }
+    fn lex(&mut self) -> Result<Vec<Tok>> {
+        while !self.eof() {
+            let ch = self.cur();
+            match ch {
+                '(' => self.tok(TokTyp::LeftParen),
+                ')' => self.tok(TokTyp::RightParen),
+                '+' => self.tok(TokTyp::Plus),
+                '*' => self.tok(TokTyp::Star),
+                '/' => self.tok(TokTyp::Slash),
+                '-' => self.tok(TokTyp::Minus),
+                '0'..'9' => self.number()?,
+                ' ' => self.advance(),
+                _ => panic!("unexpected token: {ch:?}"),
+            }
+        }
+        Ok(self.toks.clone())
+    }
+    fn number(&mut self) -> Result<()> {
+        let cur = self.pos;
+        while !self.eof() && self.cur().is_numeric() {
+            self.advance();
+        }
+        let s = self.chars[cur..self.pos]
+            .iter()
+            .collect::<String>();
+        let val = s.parse().context("parse f64")?;
+        self.toks.push(Tok {
+            lexeme: s,
+            typ: TokTyp::Number,
+            val: Some(Value::F64(val)),
+        });
+        Ok(())
+    }
+    fn tok(&mut self, typ: TokTyp) {
+        let lexeme = String::from(self.cur());
+        let tok = Tok { lexeme, typ, val: None };
+        self.toks.push(tok);
+        self.advance();
+    }
+    fn cur(&self) -> char {
+        self.chars[self.pos]
+    }
+    fn advance(&mut self) {
+        self.pos += 1;
+    }
+    fn eof(&self) -> bool {
+        self.pos >= self.chars.len()
+    }
+}
+
+fn lex(s: &str) -> Result<Vec<Tok>> {
+    Lexer::new(s).lex()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(eval("(+ 3 8)").unwrap().to_f64(), Some(11.0));
+        assert_eq!(eval("(+ 3 (* 8 4))").unwrap().to_f64(), Some(35.0));
+    }
+}
---
+++ src/bin/manual.rs
@@ -0,0 +1,113 @@
+use anyhow::{Result, bail};
+use itertools::Itertools;
+
+fn main() {
+    assert_eq!(eval("3").unwrap(), 3.0);
+    assert_eq!(eval("3 + 4").unwrap(), 7.0);
+    assert_eq!(eval("3 + 4 + 3").unwrap(), 10.0);
+    assert_eq!(eval("3 * (4 + 3)").unwrap(), 21.0);
+    assert_eq!(eval("3 * ((4/2) + (3))").unwrap(), 15.0);
+}
+
+#[derive(Debug, Clone)]
+enum Tok {
+    Num(f64),
+    Op(char),
+}
+
+struct Stack(Vec<Vec<Tok>>);
+
+impl Stack {
+    fn new() -> Self {
+        Self(vec![vec![]])
+    }
+    fn push_grp(&mut self) {
+        self.0.push(vec![]);
+    }
+    fn pop_grp(&mut self) -> Vec<Tok> {
+        self.0.pop().unwrap()
+    }
+    fn push(&mut self, tok: Tok) {
+        self.0.last_mut().unwrap().push(tok);
+    }
+    fn len(&self) -> usize {
+        self.0.last().unwrap().len()
+    }
+    fn drain(&mut self, v: usize) -> Vec<Tok> {
+        self.0
+            .last_mut()
+            .unwrap()
+            .drain(0..v)
+            .collect_vec()
+    }
+}
+
+fn eval(s: &str) -> Result<f64> {
+    let chars: Vec<char> = s.chars().collect_vec();
+    let mut pos = 0;
+    let mut stack = Stack::new();
+    while pos < chars.len() {
+        let ch = chars[pos];
+        match ch {
+            '(' => {
+                stack.push_grp();
+                pos += 1;
+            }
+            ')' => {
+                match stack.pop_grp().as_slice() {
+                    [tok] => {
+                        stack.push(tok.clone());
+                        pos += 1;
+                    }
+                    xs => bail!("bad group expr: {xs:?}"),
+                }
+            }
+            '0'..'9' => {
+                let cur = pos;
+                while pos < chars.len() && chars[pos].is_numeric() {
+                    pos += 1;
+                }
+                let num: f64 = chars[cur..pos]
+                    .iter()
+                    .collect::<String>()
+                    .parse()?;
+                stack.push(Tok::Num(num));
+            }
+            '+' | '-' | '*' | '/' => {
+                stack.push(Tok::Op(ch));
+                pos += 1;
+            }
+            ' ' | '\n' => pos += 1,
+            _ => bail!("bad ch: {ch:?}"),
+        }
+        if stack.len() >= 3 {
+            match stack.drain(3).as_slice() {
+                [Tok::Num(v1), Tok::Op(op), Tok::Num(v2)] => {
+                    let num = match op {
+                        '+' => *v1 + *v2,
+                        '-' => *v1 - *v2,
+                        '*' => *v1 * *v2,
+                        '/' => *v1 / *v2,
+                        _ => bail!("invalid op: {op:?}"),
+                    };
+                    stack.push(Tok::Num(num));
+                }
+                rest => bail!("invalid stack state: {rest:?}"),
+            }
+        }
+    }
+    match stack.pop_grp().as_slice() {
+        [Tok::Num(v)] => Ok(*v),
+        xs => bail!("invalid state: {xs:?}"),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_eval() {
+        main();
+    }
+}
